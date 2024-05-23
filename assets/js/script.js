ab05556ee4d61e22ab26bd83b2cde9100340bc8d
prashant sahu <prashantmanisahukhaga@gmail.com>
2024-05-23 21:19:11 +0530

    Implemented new feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,8 @@
-window.addEventListener('scroll', function () {
-  var navbar = document.getElementById('navbar');
- 
-  if (window.scrollY > 50) {
-    navbar.classList.add('fixed-top');
-   lo
-  } else {
-    navbar.classList.remove('fixed-top');
-    
-  }
-});
 
 $(document).ready(function () {
   $('#menu').click(function () {
-    $('#nav').toggleClass('hidden');
-    $('#menu').toggleClass('fa-x');
+    $('#nav').toggleClass('shown');
+    $('#menu').toggleClass('fa-bars fa-x ');
     
   });
 });
